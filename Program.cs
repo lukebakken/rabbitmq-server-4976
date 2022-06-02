f48be68c032057a1f9c2ac595d5b36a033b7d120
Brandon Ording <bording@gmail.com>
2022-06-02 18:33:29 -0400

    Simplify repro

--- Program.cs
+++ Program.cs
@@ -1,84 +1,27 @@
 ﻿using RabbitMQ.Client;
 
-var QuorumQueueArguments = new Dictionary<string, object> { { "x-queue-type", "quorum" } };
-var factory = new ConnectionFactory
-{
-    Uri = new Uri("amqp://guest:guest@localhost")
-};
+var factory = new ConnectionFactory();
 
-SetupQueues(factory);
-AddTestMessage(factory);
+var arguments = new Dictionary<string, object> { { "x-queue-type", "quorum" } };
+using var connection = factory.CreateConnection();
 
-using (var connection = factory.CreateConnection())
-{
-    using (var channel = connection.CreateModel())
-    {
-        channel.ConfirmSelect();
-        // adding the test message here makes it work as expected
-        //channel.BasicPublish(string.Empty, "main", channel.CreateBasicProperties(), ReadOnlyMemory<byte>.Empty);
-        channel.QueueDeclare("temp", true, false, false);
+using var channel = connection.CreateModel();
+channel.ConfirmSelect();
 
-        var messageFromMain = channel.BasicGet("main", false);
+using var secondChannel = connection.CreateModel();
+secondChannel.ConfirmSelect();
 
-        channel.BasicPublish(string.Empty, "temp", messageFromMain.BasicProperties, messageFromMain.Body);
+channel.QueueDeclare("main", true, false, false);
+channel.BasicPublish(string.Empty, "main", channel.CreateBasicProperties(), ReadOnlyMemory<byte>.Empty);
+channel.WaitForConfirmsOrDie();
 
-        channel.WaitForConfirmsOrDie();
+var message = secondChannel.BasicGet("main", false);
+secondChannel.BasicAck(message.DeliveryTag, false);
 
-        channel.BasicAck(messageFromMain.DeliveryTag, false);
+channel.QueueDelete("main");
+channel.QueueDeclare("main", true, false, false, arguments);
 
-        channel.QueueDelete("main");
-        channel.QueueDeclare("main", true, false, false, QuorumQueueArguments);
+secondChannel.BasicPublish(string.Empty, "main", secondChannel.CreateBasicProperties(), ReadOnlyMemory<byte>.Empty);
+secondChannel.WaitForConfirmsOrDie();
 
-        var messageFromTemp = channel.BasicGet("temp", false);
-
-        channel.BasicPublish(string.Empty, "main", messageFromTemp.BasicProperties, messageFromTemp.Body);
-
-        channel.WaitForConfirmsOrDie();
-
-        channel.BasicAck(messageFromTemp.DeliveryTag, false);
-
-        //using the channel from this point on will throw
-        Console.WriteLine("Num messages in main: " + channel.MessageCount("main"));
-    }
-}
-
-Console.WriteLine("Done");
-Console.ReadLine();
-
-void SetupQueues(ConnectionFactory factory)
-{
-    using (var connection = factory.CreateConnection())
-    {
-        using (var channel = connection.CreateModel())
-        {
-            try
-            {
-                channel.QueueDelete("main");
-                channel.QueueDelete("temp");
-            }
-            catch (Exception)
-            {
-            }
-        }
-    }
-
-    using (var connection = factory.CreateConnection())
-    {
-        using (var channel = connection.CreateModel())
-        {
-            channel.QueueDeclare("main", true, false, false);
-        }
-    }
-}
-
-void AddTestMessage(ConnectionFactory factory)
-{
-    using (var connection = factory.CreateConnection())
-    {
-        using (var channel = connection.CreateModel())
-        {
-            channel.BasicPublish(string.Empty, "main", channel.CreateBasicProperties(), ReadOnlyMemory<byte>.Empty);
-        }
-    }
-}
-
+channel.QueueDelete("main");
