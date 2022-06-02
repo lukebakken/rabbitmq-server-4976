982d1f9103ef509611eaaacc7a8a2cf986144119
Andreas Öhlund <andreas.ohlund@particular.net>
2022-06-02 16:35:12 +0200

    Remove exchange and bindings

--- Program.cs
+++ Program.cs
@@ -13,24 +13,28 @@ using (var connection = factory.CreateConnection())
 {
     using (var channel = connection.CreateModel())
     {
+        channel.ConfirmSelect();
         // adding the test message here makes it work as expected
         //channel.BasicPublish(string.Empty, "main", channel.CreateBasicProperties(), ReadOnlyMemory<byte>.Empty);
-        channel.QueueDeclare("temp", true, false, false, QuorumQueueArguments);
-        channel.QueueBind("temp", "main", string.Empty);
-        channel.QueueUnbind("main", "main", string.Empty);
+        channel.QueueDeclare("temp", true, false, false);
 
         var messageFromMain = channel.BasicGet("main", false);
 
         channel.BasicPublish(string.Empty, "temp", messageFromMain.BasicProperties, messageFromMain.Body);
+
+        channel.WaitForConfirmsOrDie();
+
         channel.BasicAck(messageFromMain.DeliveryTag, false);
 
         channel.QueueDelete("main");
         channel.QueueDeclare("main", true, false, false, QuorumQueueArguments);
-        channel.QueueBind("main", "main", string.Empty);
 
         var messageFromTemp = channel.BasicGet("temp", false);
 
         channel.BasicPublish(string.Empty, "main", messageFromTemp.BasicProperties, messageFromTemp.Body);
+
+        channel.WaitForConfirmsOrDie();
+
         channel.BasicAck(messageFromTemp.DeliveryTag, false);
 
         //using the channel from this point on will throw
@@ -50,6 +54,7 @@ void SetupQueues(ConnectionFactory factory)
             try
             {
                 channel.QueueDelete("main");
+                channel.QueueDelete("temp");
             }
             catch (Exception)
             {
@@ -62,8 +67,6 @@ void SetupQueues(ConnectionFactory factory)
         using (var channel = connection.CreateModel())
         {
             channel.QueueDeclare("main", true, false, false);
-            channel.ExchangeDeclare("main", ExchangeType.Fanout, true);
-            channel.QueueBind("main", "main", string.Empty);
         }
     }
 }
